2081a2ba327311384e83ec664d1b2b91846244ef
Nikolaj Baer <nikolaj.baer@gmail.com>
2020-04-04 14:58:28 -0700

    remove redundancy in project row

--- src/components/ProjectRow.vue
+++ src/components/ProjectRow.vue
@@ -6,17 +6,6 @@
         <div class="project-description" v-if="project.description">
             {{ project.description }} 
         </div>
-        <div class="metrics">
-            <span class="passed-metric" v-if="project.topics" :title="project.topics">
-                <i class="fa fa-check"></i> Topics
-            </span>
-            <span class="passed-metric" v-if="project.description" :title="project.description">
-                <i class="fa fa-check"></i> Description
-            </span>
-            <a v-if="project.git_url" :href="project.code_url">
-                <i class="fa fa-github"></i>
-            </a>
-        </div>
         <div class='other-topics'>
             <span v-for="(t,index) in  other_topics(project.topics)" v-bind:key="index">
                 <strong v-if="index == 0">Other topics:</strong>
@@ -34,10 +23,10 @@
         </div>
         <div class="metrics">
             <span class="passed-metric" v-if="project.topics" :title="project.topics">
-                <i class="fa fa-check"></i> Github topics
+                <i class="fa fa-check"></i> topics
             </span>
             <span class="passed-metric" v-if="project.description" :title="project.description">
-                <i class="fa fa-check"></i> Github description
+                <i class="fa fa-check"></i> description
             </span>
         </div>
         <router-link :to="`/project/${project.brigade_slug}/${project.slug}`" class="badge badge-primary">Improve your project's visibility and impact</router-link>
