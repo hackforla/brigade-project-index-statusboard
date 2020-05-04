3eec39d2cbe1ad6a2b36da0015f23ea9993353cf
Melanie <melanie.mazanec@gmail.com>
2020-05-04 00:33:38 -0400

    Make nav bar

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,73 +1,58 @@
 <template>
-
     <div class="content">
-        <header>
-            <h1><router-link to="/">Brigade Project Index</router-link></h1>
-            <h2>status board</h2>
+        <Nav></Nav>
+        <main>
             <div class="loader" v-if="loading">
                 <div class="spinner-border" role="status">
                     <span class="sr-only">loading...</span>
                 </div>
                 Loading {{ loading }}
             </div>
-        </header>
-        <router-view class="main-router-view"></router-view>
-        <div class="leader-link">
-            <router-link class="btn btn-primary" to="/">Map</router-link>
-            <router-link class="btn btn-primary" to="/leaders">Brigades</router-link>
-            <router-link class="btn btn-primary" to="/topics">Projects by Topic</router-link>
-            <router-link class="btn btn-warning" to="/topics/covid-19">Covid-19 Related Projects</router-link> 
-        </div>
+            <router-view class="main-router-view"></router-view>
+        </main>
     </div>
 </template>
 
 <script>
+import Nav from "./Nav.vue";
 export default {
+    components: {
+        Nav
+    },
     data() {
         return {
             last_check: null,
-            map_filter_topic: "covid-19",
+            map_filter_topic: "covid-19"
         };
     },
     computed: {
-        loading(){
+        loading() {
             return this.$store.getters.loading;
         },
-        dev_site(){
+        dev_site() {
             return this.$store.getters.is_dev_site;
         }
     },
-    created(){
+    created() {
         this.last_check = new Date();
         this.$store.dispatch("load_all");
-        window.setInterval(this.checkForUpdates, 60*1000*10); // Check for updates every 10 minutes
+        window.setInterval(this.checkForUpdates, 60 * 1000 * 10); // Check for updates every 10 minutes
     },
     methods: {
         checkForUpdates() {
             this.$store.dispatch("check_for_updates", this.last_check);
             this.last_check = new Date();
         },
-        filter_map(topic){
-            console.log("filtering map by" ,topic)
+        filter_map(topic) {
+            console.log("filtering map by", topic);
             this.map_filter_topic = topic;
         }
     }
-}
+};
 </script>
 
 <style>
-    header {
-        position: absolute;
-        top: 10px;
-        width: 100%;
-        text-align: center;
-    } 
-    .leader-link {
-        position: fixed;
-        left: 8px;
-        bottom: 8px;
-    }
-    .main-router-view {
-        margin-bottom: 80px;
-    }
+.main-router-view {
+    margin-bottom: 80px;
+}
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,52 @@
+<template>
+    <nav>
+        <div class="home">
+            <router-link to="/">Brigade Project Index</router-link>
+        </div>
+        <ul>
+            <li>
+                <router-link to="/">Map</router-link>
+            </li>
+            <li>
+                <router-link to="/leaders">Brigades</router-link>
+            </li>
+            <li>
+                <router-link to="/topics">Topics</router-link>
+            </li>
+            <li>
+                <router-link to="/topics/covid-19">Covid-19 Related Projects</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Nav"
+};
+</script>
+
+<style>
+nav {
+    top: 0;
+    width: 100%;
+    padding: 1rem 1rem 0 1rem;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid #007bff;
+    background-color: white;
+}
+nav .home {
+    width: fit-content;
+    vertical-align: baseline;
+}
+nav ul {
+    list-style: none;
+    display: flex;
+    align-items: flex-end;
+    margin: unset;
+}
+nav ul li {
+    padding-left: 1rem;
+}
+</style>
