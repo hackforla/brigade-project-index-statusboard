05c1d87df5d194c385c3d2e1645ade3704267a2f
Melanie <melanie.mazanec@gmail.com>
2020-05-30 23:49:22 -0400

    Remove unnecessary comment

--- src/pages/Brigades.js
+++ src/pages/Brigades.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Brigades() {
   return (
     <>
-      {/* TODO: this should be an h1 and the nav bar should just use a div */}
       <h2>Brigades page!</h2>
       {/* List projects by brigades that are shown on accompanying map */}
       {/* When map zooms or moves, re-filter geographically */}
