632b84621055e99462ef39fe3168c554db80ca5c
Melanie <melanie.mazanec@gmail.com>
2020-05-31 14:04:35 -0400

    Debug

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,54 +2,50 @@ import React, { useCallback, useState } from 'react';
 import { zoom as d3zoom, zoomIdentity, zoomTransform } from 'd3-zoom';
 import { select, event, mouse } from 'd3-selection';
 import { feature, mesh } from 'topojson-client';
-import { geoPath as path } from 'd3-geo';
+import { geoPath } from 'd3-geo';
 // TODO: FIND THE JSON FILE THAT INCLUDES PUERTO
 import us from '../../assets/states-albers-10m';
 import './Map.scss';
 
 // Taken from https://observablehq.com/@d3/zoom-to-bounding-box
 export default function Map({}) {
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-
   const zoom = d3zoom().scaleExtent([1, 8]).on('zoom', zoomed);
+  const path = geoPath();
   let svg, statePathsGroup;
+  let width = 0;
+  let height = 0;
 
   const svgRef = useCallback((svgNode) => {
-    if (svgNode !== null) {
-      // Set height and width
-      const svgDimensions = svgNode.getBoundingClientRect();
-      setWidth(svgDimensions.width);
-      setHeight(svgDimensions.height);
+    if (!svgNode) return;
+    // Set height and width
+    const svgDimensions = svgNode.getBoundingClientRect();
+    width = svgDimensions.width;
+    height = svgDimensions.height;
 
-      // Do all of the d3 things once the svg node exists
-      svg = select(svgNode);
-      statePathsGroup = svg.append('g');
+    // Do all of the d3 things once the svg node exists
+    svg = select(svgNode);
+    statePathsGroup = svg.append('g');
 
-      statePathsGroup
-        .append('g')
-        .attr('class', 'states')
-        .selectAll('path')
-        .data(feature(us, us.objects.states).features)
-        .join('path')
-        // TODO: add keyboard nav
-        .on('click', clicked)
-        .attr('d', path())
-        // TODO: in example path was not called, but doesn't render unless it's called-- but calling it here breaks the zoom function :/
-        .append('title')
-        .text((d) => d.properties.name);
+    statePathsGroup
+      .append('g')
+      .attr('class', 'states-group')
+      .selectAll('path')
+      .data(feature(us, us.objects.states).features)
+      .join('path')
+      // TODO: add keyboard nav
+      .on('click', clicked)
+      .attr('d', path)
+      .append('title')
+      .text((d) => d.properties.name);
 
-      statePathsGroup
-        // TODO: WHAT DOES THIS EVEN DO???
-        .append('path')
-        .attr('fill', 'none')
-        .attr('stroke', 'white')
-        .attr('stroke-linejoin', 'round')
-        .attr('d', path(mesh(us, us.objects.states, (a, b) => a !== b)));
+    statePathsGroup
+      .append('path')
+      .attr('class', 'state-border')
+      .attr('stroke-linejoin', 'round')
+      .attr('d', path(mesh(us, us.objects.states, (a, b) => a !== b)));
 
-      svg.call(zoom);
-      reset();
-    }
+    svg.call(zoom);
+    reset();
   }, []);
 
   return (
