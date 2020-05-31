e478725a637698b280c1e58525c45747eab3e9ae
Melanie <melanie.mazanec@gmail.com>
2020-05-30 23:14:30 -0400

    Make click target bigger and add focus behavior

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Nav.css';
+import './Nav.scss';
 
 function Nav() {
   return (
--- src/pages/Brigades.js
+++ src/pages/Brigades.js
@@ -5,6 +5,9 @@ function Brigades() {
     <>
       {/* TODO: this should be an h1 and the nav bar should just use a div */}
       <h2>Brigades page!</h2>
+      {/* List projects by brigades that are shown on accompanying map */}
+      {/* When map zooms or moves, re-filter geographically */}
+      {/* Accessible filter by region, state, or a single brigade */}
       <p>This is the brigade search will go.</p>
     </>
   );
