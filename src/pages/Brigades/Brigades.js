a3cb1a20496f120b2965729029127d917206dfb6
Melanie <melanie.mazanec@gmail.com>
2020-06-03 00:43:34 -0400

    Start to make form components

--- src/pages/Brigades/Brigades.js
+++ src/pages/Brigades/Brigades.js
@@ -30,28 +30,34 @@ function Brigades() {
     getData();
   }, []);
 
+  console.log(brigadeData);
+
   return (
     <>
       <h2>Projects by brigade, state, or geographic area</h2>
-      <p>
-        Move the map or zoom in to filter by projects in a geographic area.
-        Click a state on the map or use the dropdown to filter projects by
-        state. Click a brigade or select from the dropdown to look for projects
-        owned by a single brigade.
-      </p>
       {/* List projects by brigades that are shown on accompanying map */}
       {/* When map zooms or moves, re-filter geographically */}
       {/* Accessible filter by region, state, or a single brigade */}
       <div className="brigades-page-content">
         <Map brigadeData={brigadeData} />
         <div className="map-info">
+          <p>
+            Move the map or zoom in to filter by projects in a geographic area.
+            Click a state on the map or use the dropdown to filter projects by
+            state. Click a brigade or select from the dropdown to look for
+            projects owned by a single brigade.
+          </p>
           {/* TODO: FIGURE OUT WHAT GOES HERE */}
-          <div>
-            <label>Select a state</label>
-            <select>
-              <option>state</option>
-            </select>
-          </div>
+          {brigadeData && (
+            <div>
+              <label htmlFor="state-picker">Select a brigade</label>
+              <select id="state-picker">
+                {brigadeData.map((brigade) => (
+                  <option>{brigade.name}</option>
+                ))}
+              </select>
+            </div>
+          )}
         </div>
       </div>
       <ProjectsTable projects={projects} />
