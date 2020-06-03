930775aac0795eaada828df1ed5de5dca0b3a3bc
Melanie <melanie.mazanec@gmail.com>
2020-06-03 01:04:26 -0400

    Adjust layout

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 function About() {
   return (
-    <>
+    <div className="text-content">
       <h2>About page!</h2>
-      <p>This is the about page.</p>
-    </>
+      <p>
+        The Brigade Project Index is a collaborative project involving core
+        contributorts from across many brigades. We design, develop, and
+        maintain a set of tools and practices for mapping out all the work being
+        done on projects across the entire global network of civic hacking
+        organizations and Code for America brigades. Our core goal is to publish
+        a rich and constantly-updated database of projects that any number of
+        network projects can use as a data source to help us get better
+        connected.
+      </p>
+    </div>
   );
 }
 
