704abf423068f7fb5f12cc1f07cb55b648dd6d42
Melanie <melanie.mazanec@gmail.com>
2020-06-02 23:26:02 -0400

    Add projects table

--- src/components/ProjectsTable/ProjectsTable.js
+++ src/components/ProjectsTable/ProjectsTable.js
@@ -1,20 +1,152 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { memo } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useTable, usePagination } from 'react-table';
 
+// https://github.com/tannerlinsley/react-table/blob/master/docs/examples/simple.md
 export default function ProjectsTable({ projects }) {
-  // This is an extremely rudimentary version of this component for the sake of getting something into existence
-  // and being able to use it in the brigade view
-  // We should probably use react-table
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Project name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Description',
+        accessor: 'description',
+      },
+      {
+        Header: 'Brigade',
+        accessor: 'brigade.name',
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data: projects,
+      initialState: { pageIndex: 2 },
+    },
+    usePagination
+  );
+
   console.log(projects[0]);
+  // <NavLink to={`/projects/${project.name}`}>
+  //   {project.name}
+  // </NavLink>;
+
   return (
     <div className="projects-table">
-      {projects.map((project, i) => {
-        return (
-          <div key={i}>
-            <NavLink to={`/projects/${project.name}`}>{project.name}</NavLink>
-          </div>
-        );
-      })}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            console.log(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+      <div className="pagination">
+        <button
+          type="button"
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}
+        >
+          {'<<'}
+        </button>{' '}
+        <button
+          type="button"
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          {'<'}
+        </button>{' '}
+        <button
+          type="button"
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
+          {'>'}
+        </button>{' '}
+        <button
+          type="button"
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        >
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const p = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(p);
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((_pageSize) => (
+            <option key={_pageSize} value={_pageSize}>
+              Show {_pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
