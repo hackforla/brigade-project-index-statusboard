1ec693164f9665c48952b53f1d98897178f9edab
Melanie <melanie.mazanec@gmail.com>
2020-06-19 20:05:28 -0400

    Use leaflet for map

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import '../commonFormControlStyles.scss';
 import './Button.scss';
 
-export default function Button({ text, onClick, disabled }) {
+export default function Button({ text, onClick, disabled, linkButton }) {
   // TODO: also make it possible to return link styled like a button
 
   return (
     <button
-      className="button form-control"
+      className={`button ${linkButton ? ' link-button' : ' form-control'}`}
       type="button"
       onClick={onClick}
       disabled={disabled}
@@ -20,10 +20,12 @@ export default function Button({ text, onClick, disabled }) {
 
 Button.defaultProps = {
   disabled: false,
+  linkButton: false,
 };
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
+  linkButton: PropTypes.bool,
 };
