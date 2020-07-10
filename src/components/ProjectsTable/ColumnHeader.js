302b50a63f47125d191226519e2ad6a26b93ccd0
Melanie <melanie.mazanec@gmail.com>
2020-07-09 20:58:38 -0400

    Position sort arrows

--- src/components/ProjectsTable/ColumnHeader.js
+++ src/components/ProjectsTable/ColumnHeader.js
@@ -25,7 +25,7 @@ export default function ColumnHeader({ column }) {
               className="sort-button"
               onClick={sortProps ? sortProps.onClick : undefined}
             >
-              <div>
+              <>
                 {column.isSorted && (
                   <span className="sr-only">
                     {column.isSortedDesc ? 'Descending' : 'Ascending'}
@@ -43,7 +43,7 @@ export default function ColumnHeader({ column }) {
                     })}
                   />
                 </div>
-              </div>
+              </>
             </Button>
           </div>
         )}
