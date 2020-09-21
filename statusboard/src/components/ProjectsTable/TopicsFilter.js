62ab3fd48603a4f4a3cfba2d42c6547cc811fb00
Tom Dooner <tdooner@codeforamerica.org>
2020-09-21 13:05:20 -0700

    Put the most frequent topics at the top of the list

--- statusboard/src/components/ProjectsTable/TopicsFilter.js
+++ statusboard/src/components/ProjectsTable/TopicsFilter.js
@@ -4,16 +4,29 @@ import React from 'react';
 import Select from '../Select/Select';
 
 export default function TopicFilter({ column: { setFilter, preFilteredRows } }) {
-  const allTopics = new Set(
-    preFilteredRows.flatMap((row) => (row.values.Topics || '').split(', '))
-  );
+  const topicsByFrequency = {};
+
+  preFilteredRows.forEach((row) => {
+    const topics = (row.values.Topics || '').split(', ');
+    if (topics.length === 1 && topics[0] === "") {
+      return;
+    }
+
+    topics.forEach((topic) =>
+      topicsByFrequency[topic] = (topicsByFrequency[topic] || 0) + 1
+    )
+  });
+
+  const sortedTopics = Object.entries(topicsByFrequency)
+    .sort((a, b) => b[1] - a[1])
+    .map((topicAndCount) => topicAndCount[0]);
 
   return (
     <Select
       id="topic"
       label=""
       emptyOptionText=""
-      options={Array.from(allTopics).sort()}
+      options={sortedTopics}
       onChange={(e) => setFilter(e.target.value)}
     />
   );
