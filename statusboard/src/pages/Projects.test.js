3a90ac8d6e62ada9f7efda6bb1ee22938e6efd6b
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2021-07-22 15:08:26 -0400

    Fixes failing unit test in `Projects.test.js`

--- statusboard/src/pages/Projects.test.js
+++ statusboard/src/pages/Projects.test.js
@@ -1,11 +1,13 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import React from 'react';
+import { StaticRouter } from 'react-router-dom';
 
 import { render, screen } from '@testing-library/react';
 
-import Projects from './Projects';
+import Projects from './Projects/Projects';
 import { SAMPLE_BRIGADE } from '../utils/utils.test';
+import { BrigadeDataContextProvider } from '../contexts/BrigadeDataContext';
 
 const fakeServer = setupServer(
   rest.get('/api/data.json', (req, res, ctx) => {
@@ -18,10 +20,22 @@ beforeAll(() => fakeServer.listen());
 afterAll(() => fakeServer.close());
 afterEach(() => fakeServer.resetHandlers());
 
+function RouteWithContext({ children, location }) {
+  return (
+    <BrigadeDataContextProvider>
+      <StaticRouter location={location}>{children}</StaticRouter>
+    </BrigadeDataContextProvider>
+  );
+}
+
 describe('Page: <Projects>', () => {
   it('renders before and after projects have loaded', async () => {
-    render(<Projects />);
-    expect(await screen.findByText(/Loading/i)).toBeInTheDocument();
+    render(
+      <RouteWithContext location="/projects?timeRange=all%20time">
+        <Projects />
+      </RouteWithContext>
+    );
+    expect(await screen.getByText(/Loading/i)).toBeInTheDocument();
     expect(await screen.findByText(/311-index/)).toBeInTheDocument();
   });
 });
