03a7e5f546aa172ffaace905d568239323f61296
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2021-08-19 10:14:55 -0400

    Add Footer

---
+++ statusboard/src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.scss';
+
+const submitBugReport =
+  'https://github.com/codeforamerica/brigade-project-index-statusboard/issues/new?assignees=&labels=&template=bug_report.md&title=';
+const submitFeatureRequest =
+  'https://github.com/codeforamerica/brigade-project-index-statusboard/issues/new?assignees=&labels=&template=feature_request.md&title=';
+const githubRepository =
+  'https://github.com/codeforamerica/brigade-project-index-statusboard';
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className={'App-footer'}>
+      <p>
+        Submit a <a href={submitBugReport}>bug report</a> or{' '}
+        <a href={submitFeatureRequest}>feature requests</a> to our GitHub{' '}
+        <a href={githubRepository}>repository</a>
+      </p>
+    </footer>
+  );
+}
