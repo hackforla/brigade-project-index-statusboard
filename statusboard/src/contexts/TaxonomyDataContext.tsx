00b95cc93fc30b6c61dadc0afbc35d2c62123791
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2021-11-01 17:30:44 -0400

    formatting

--- statusboard/src/contexts/TaxonomyDataContext.tsx
+++ statusboard/src/contexts/TaxonomyDataContext.tsx
@@ -37,31 +37,31 @@ const TaxonomyDataContextProvider = ({
     const getData = async () => {
 
       try {
-      const issues_resp = await axios.get(`https://api.taxonomy.sandbox.k8s.brigade.cloud/taxonomy?category=Issues`)
-      const issues = issues_resp.data;
-          // only take the children of the first object, this is the detailed list of taxonomy items
+        const issues_resp = await axios.get(`https://api.taxonomy.sandbox.k8s.brigade.cloud/taxonomy?category=Issues`)
+        const issues = issues_resp.data;
+        // only take the children of the first object, this is the detailed list of taxonomy items
 
-      const taxonomyIssues:TaxonomyList = issues[0].children;
-      const issuesMapLocal = new Map<string, string[]>(taxonomyIssues.map(key => [key.text, key.children]));
-      setIssuesMap(issuesMapLocal);
+        const taxonomyIssues: TaxonomyList = issues[0].children;
+        const issuesMapLocal = new Map<string, string[]>(taxonomyIssues.map(key => [key.text, key.children]));
+        setIssuesMap(issuesMapLocal);
       }
-      catch(error){
-       setIsTaxonomyError(true);
+      catch (error) {
+        setIsTaxonomyError(true);
       };
-     
-     try {
-      const paa_resp = await axios.get(`https://api.taxonomy.sandbox.k8s.brigade.cloud/taxonomy?category=Priority-Action-Areas`);
-      const _paa_data = paa_resp.data
-      const priorityAreas = _paa_data;
 
-      // only take the children of the first object, this is the detailed list of taxonomy items
-      const taxonomyPriorityAreas:TaxonomyList = priorityAreas[0].children;
-      const priorityAreasMapLocal = new Map<string, string[]>(taxonomyPriorityAreas.map(key => [key.text, key.children]));
-      setPriorityAreasMap(priorityAreasMapLocal);
-     }
-     catch(error){
-       setIsTaxonomyError(true);
-     }
+      try {
+        const paa_resp = await axios.get(`https://api.taxonomy.sandbox.k8s.brigade.cloud/taxonomy?category=Priority-Action-Areas`);
+        const _paa_data = paa_resp.data
+        const priorityAreas = _paa_data;
+
+        // only take the children of the first object, this is the detailed list of taxonomy items
+        const taxonomyPriorityAreas: TaxonomyList = priorityAreas[0].children;
+        const priorityAreasMapLocal = new Map<string, string[]>(taxonomyPriorityAreas.map(key => [key.text, key.children]));
+        setPriorityAreasMap(priorityAreasMapLocal);
+      }
+      catch (error) {
+        setIsTaxonomyError(true);
+      }
     }
     getData();
     // Disabling bc data length isn't going to change outside of this hook
